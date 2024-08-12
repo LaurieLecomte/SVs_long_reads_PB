f9ba8f366cf67d362ab2eda9860baa18ddacb573
LaurieLecomte <lalec31@manitou.ibis.ulaval.ca>
2024-08-12 16:12:39 -0400

    added .ccs to BAM variable

--- 01_scripts/03.2_nanovar_refine.sh
+++ 01_scripts/03.2_nanovar_refine.sh
@@ -4,10 +4,7 @@
 # Launch on valeria in module collection with jasmine and iris
 
 # manitou
-# parallel -a 02_infos/ind_ONT.txt -j 4 srun -c 10 -p medium --time=3-00:00:00 -J 03.2_nanovar_refine_{} --mem=80G -o log/02.2_nanovar_refine_{}_%j.log /bin/sh ./01_scripts/03.2_nanovar_refine.sh {} &
-
-# valeria
-# parallel -a 02_infos/ind_ONT.txt -j 4 srun -c 10 -p ibis_medium --time=7-00:00:00 -J 03.2_nanovar_refine_{} --mem=100G -o log/03.2_nanovar_refine_{}_%j.log /bin/sh ./01_scripts/03.2_nanovar_refine.sh {} &
+# parallel -a 02_infos/ind_PB.txt -j 4 srun -c 10 -p medium --time=3-00:00:00 -J 03.2_nanovar_refine_{} --mem=80G -o log/02.2_nanovar_refine_{}_%j.log /bin/sh ./01_scripts/03.2_nanovar_refine.sh {} &
 
 
 # VARIABLES
@@ -22,6 +19,8 @@ FILT_DIR="07_filtered"
 
 CPU=10
 
+BAM="$BAM_DIR/"$SAMPLE".ccs.bam"
+
 # LOAD REQUIRED MODULES
 #module load gcc python/3.10 bcftools/1.13 samtools/1.15 minimap2/2.24 blast+/2.13.0 bedtools/2.30.0 java/17.0.2 racon/1.4.13
 
@@ -34,7 +33,7 @@ echo "$CALLS_DIR/nanovar/$SAMPLE/"$SAMPLE"_PASS_correctedDUPs.vcf" > $CALLS_DIR/
 jasmine file_list=$CALLS_DIR/nanovar/$SAMPLE/"$SAMPLE".txt out_dir=$CALLS_DIR/nanovar/$SAMPLE genome_file=$GENOME out_file=$CALLS_DIR/nanovar/$SAMPLE/"$SAMPLE"_PASS_correctedDUPs_dupToIns.vcf --dup_to_ins --preprocess_only
 
 # 3. Refine with iris
-iris genome_in=$GENOME vcf_in=$CALLS_DIR/nanovar/$SAMPLE/"$SAMPLE"_PASS_correctedDUPs_dupToIns.vcf reads_in=$BAM_DIR/"$SAMPLE".bam vcf_out=$CALLS_DIR/nanovar/"$SAMPLE"_PASS_refined_dupToIns.vcf --out_dir=$CALLS_DIR/nanovar/$SAMPLE --keep_long_variants --also_deletions --threads=$CPU
+iris genome_in=$GENOME vcf_in=$CALLS_DIR/nanovar/$SAMPLE/"$SAMPLE"_PASS_correctedDUPs_dupToIns.vcf reads_in=$BAM vcf_out=$CALLS_DIR/nanovar/"$SAMPLE"_PASS_refined_dupToIns.vcf --out_dir=$CALLS_DIR/nanovar/$SAMPLE --keep_long_variants --also_deletions --threads=$CPU
 
 
 # Clean up
