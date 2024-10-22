bc35682a7d2ffb209caef8d0a46e7614de555bbb
LaurieLecomte <lalec31@manitou.ibis.ulaval.ca>
2024-10-22 13:47:22 -0400

    corrected across sample merge scripts 0X.3 to use a sample list

--- 01_scripts/01.3_sniffles_merge.sh
+++ 01_scripts/01.3_sniffles_merge.sh
@@ -3,10 +3,7 @@
 # Merge refined sniffles SVs across samples with Jasmine
 
 # manitou
-# srun -c 8 -p medium --time=2-00:00:00 -J 01.3_sniffles_merge --mem=100G -o log/01.3_sniffles_merge_%j.log /bin/sh ./01_scripts/01.3_sniffles_merge.sh &
-
-# valeria
-# srun -c 8 -p ibis_medium --time=2-00:00:00 -J 01.3_sniffles_merge --mem=100G -o log/01.3_sniffles_merge_%j.log /bin/sh ./01_scripts/01.3_sniffles_merge.sh &
+# srun -c 8 -p medium --time=2-00:00:00 -J 01.3_sniffles_merge --mem=100G -o log/01.3_sniffles_merge_%j.log /bin/sh ./01_scripts/01.3_sniffles_merge.sh 02_infos/PB_samples.txt &
 
 # VARIABLES
 GENOME="03_genome/genome.fasta"
@@ -18,13 +15,19 @@ FILT_DIR="07_filtered"
 
 CPU=8
 
-VCF_LIST_sniffles="$CALLS_DIR/sniffles/vcf_list.txt" # list of sniffles VCFs files
+SAMPLE_LIST=$1
+VCF_LIST="$CALLS_DIR/sniffles/vcf_list.txt" # list of sniffles VCFs files
 
 # 1. Make a list of sniffles sample VCF files to merge
-ls -1 $CALLS_DIR/sniffles/*refined_dupToIns.vcf > $VCF_LIST_sniffles
+if [[ -f $VCF_LIST ]]
+then
+  rm $VCF_LIST
+fi
+less $SAMPLE_LIST | while read SAMPLE; do ls $CALLS_DIR/sniffles/*refined_dupToIns.vcf | grep $SAMPLE >> $VCF_LIST ; done
+
 
 # 2. Merge VCFs across samples 
-jasmine file_list=$VCF_LIST_sniffles out_file="$MERGED_DIR/sniffles/sniffles_PASS_PRECISE_refined.vcf" out_dir=$MERGED_DIR/sniffles genome_file=$GENOME --ignore_strand --mutual_distance --allow_intrasample --output_genotypes --threads=$CPU
+jasmine file_list=$VCF_LIST out_file="$MERGED_DIR/sniffles/sniffles_PASS_PRECISE_refined.vcf" out_dir=$MERGED_DIR/sniffles genome_file=$GENOME --ignore_strand --mutual_distance --allow_intrasample --output_genotypes --threads=$CPU
 
 # 3. Convert INSs back to DUPs (out_file is the VCF to be postprocessed, will be modified in situ)
 jasmine out_file="$MERGED_DIR/sniffles/sniffles_PASS_PRECISE_refined.vcf" out_dir=$MERGED_DIR/sniffles genome_file=$GENOME --threads=$CPU --dup_to_ins --postprocess_only
--- 01_scripts/03.3_nanovar_merge.sh
+++ 01_scripts/03.3_nanovar_merge.sh
@@ -3,10 +3,7 @@
 # Merge refined sniffles SVs across samples with Jasmine
 
 # manitou
-# srun -c 8 -p medium --time=2-00:00:00 -J 03.3_nanovar_merge --mem=100G -o log/03.3_nanovar_merge_%j.log /bin/sh ./01_scripts/03.3_nanovar_merge.sh &
-
-# valeria
-# srun -c 8 -p ibis_medium --time=2-00:00:00 -J 03.3_nanovar_merge --mem=100G -o log/03.3_nanovar_merge_%j.log /bin/sh ./01_scripts/03.3_nanovar_merge.sh &
+# srun -c 8 -p medium --time=2-00:00:00 -J 03.3_nanovar_merge --mem=100G -o log/03.3_nanovar_merge_%j.log /bin/sh ./01_scripts/03.3_nanovar_merge.sh 02_infos/PB_samples.txt &
 
 # VARIABLES
 GENOME="03_genome/genome.fasta"
@@ -18,13 +15,18 @@ FILT_DIR="07_filtered"
 
 CPU=8
 
-VCF_LIST_NANOVAR="$CALLS_DIR/nanovar/vcf_list.txt" # list of nanovar VCFs files
+SAMPLE_LIST=$1
+VCF_LIST="$CALLS_DIR/nanovar/vcf_list.txt" # list of nanovar VCFs files
 
 # 1. Make a list of nanovar sample VCF files to merge
-ls -1 $CALLS_DIR/nanovar/*refined_dupToIns.vcf > $VCF_LIST_NANOVAR
+if [[ -f $VCF_LIST ]]
+then
+  rm $VCF_LIST
+fi
+less $SAMPLE_LIST | while read SAMPLE; do ls $CALLS_DIR/nanovar/*refined_dupToIns.vcf | grep $SAMPLE >> $VCF_LIST ; done
 
 # 2. Merge VCFs across samples 
-jasmine file_list=$VCF_LIST_NANOVAR out_file="$MERGED_DIR/nanovar/nanovar_PASS_refined.vcf" out_dir=$MERGED_DIR/nanovar genome_file=$GENOME --ignore_strand --mutual_distance --allow_intrasample --output_genotypes --threads=$CPU
+jasmine file_list=$VCF_LIST out_file="$MERGED_DIR/nanovar/nanovar_PASS_refined.vcf" out_dir=$MERGED_DIR/nanovar genome_file=$GENOME --ignore_strand --mutual_distance --allow_intrasample --output_genotypes --threads=$CPU
 
 # 3. Convert INSs back to DUPs (out_file is the VCF to be postprocessed, will be modified in situ)
 jasmine out_file="$MERGED_DIR/nanovar/nanovar_PASS_refined.vcf" out_dir=$MERGED_DIR/svim out_dir=$MERGED_DIR/nanovar genome_file=$GENOME --threads=$CPU --dup_to_ins --postprocess_only
